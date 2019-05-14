4199177c7046ec828f7bc7be0b5a758e2fe5c17c
Asif Ahmed <asif@joymode.com>
2019-05-14 13:36:46 -0700

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h2>Backlog</h2>
+      <div v-html="backlog"></div>
+    </div>
+    <div class="container">
+      <h2>Current Sprint</h2>
+      <div v-html="currentSprint"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import showdown from "showdown";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  data() {
+    return {
+      backlog: null,
+      currentSprint: null
+    };
+  },
+  async created() {
+    const gist = await axios.get(
+      `https://api.github.com/gists/${process.env.VUE_APP_GIST_ID}`,
+      {
+        headers: {
+          Authorization: `token ${process.env.VUE_APP_GIST_GITHUB_TOKEN}`
+        }
+      }
+    );
+
+    const converter = new showdown.Converter();
+    converter.setFlavor("github");
+
+    this.backlog = converter.makeHtml(gist.data.files["backlog.md"].content);
+    this.currentSprint = converter.makeHtml(
+      gist.data.files["current.md"].content
+    );
   }
-}
+};
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: white;
+  margin-top: 10px;
+  background-color: #424242;
+}
+a {
+  color: #88e188;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+}
+
+.container {
+  flex: 50%;
 }
 </style>
